ad9aeb3f3e89ad9443fbf371eb7d5a86d8c1d869
Jim Pick <jim@jimpick.com>
2022-12-04 20:56:27 -0800

    Add kubernetes provider config

--- prototypes/localnet-farm-2/terraform/main.tf
+++ prototypes/localnet-farm-2/terraform/main.tf
@@ -313,6 +313,16 @@ module "lb_role" {
   }
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", local.name]
+    command     = "aws"
+  }
+}
+
 resource "kubernetes_service_account" "service-account" {
   metadata {
     name = "aws-load-balancer-controller"
