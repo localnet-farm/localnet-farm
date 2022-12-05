9ec4dfc7dde13317808053cf0d8f3fe43c5aff7c
Jim Pick <jim@jimpick.com>
2022-12-04 19:52:59 -0800

    Add role and service account

--- prototypes/localnet-farm-2/terraform/main.tf
+++ prototypes/localnet-farm-2/terraform/main.tf
@@ -299,4 +299,33 @@ resource "aws_security_group" "eks" {
   #}, var.tags)
 }
 
+module "lb_role" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name = "${local.name}_eks_lb"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "kubernetes_service_account" "service-account" {
+  metadata {
+    name = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+        "app.kubernetes.io/name"= "aws-load-balancer-controller"
+        "app.kubernetes.io/component"= "controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.lb_role.iam_role_arn
+      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+    }
+  }
+}
+
 
