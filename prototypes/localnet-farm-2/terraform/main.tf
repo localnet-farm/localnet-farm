48f4b49a3a4c14f593b1c6c58ec123f99637fa4d
Jim Pick <jim@jimpick.com>
2022-12-04 21:03:22 -0800

    Change source of variables

--- prototypes/localnet-farm-2/terraform/main.tf
+++ prototypes/localnet-farm-2/terraform/main.tf
@@ -314,11 +314,11 @@ module "lb_role" {
 }
 
 provider "kubernetes" {
-  host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(var.cluster_ca_cert)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", local.name]
     command     = "aws"
   }
 }
