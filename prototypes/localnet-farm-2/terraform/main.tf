c9339a6e554657e30802f5f0daab2edae62b0f75
Jim Pick <jim@jimpick.com>
2022-12-04 21:00:29 -0800

    Update API version for kubernetes provider config

--- prototypes/localnet-farm-2/terraform/main.tf
+++ prototypes/localnet-farm-2/terraform/main.tf
@@ -314,11 +314,11 @@ module "lb_role" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_ca_cert)
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", local.name]
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
     command     = "aws"
   }
 }
