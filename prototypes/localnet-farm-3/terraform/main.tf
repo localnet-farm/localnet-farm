cf060b3c905eaf85df97c6c2af3e3f82ec5e97a1
Jim Pick <jim@jimpick.com>
2022-12-12 12:23:11 -0800

    Add knative-serving Fargate profile

--- prototypes/localnet-farm-3/terraform/main.tf
+++ prototypes/localnet-farm-3/terraform/main.tf
@@ -94,6 +94,27 @@ module "eks" {
 	GithubOrg  = "jimpick"
       }
     }
+
+    knative_serving = {
+      name = "knative-serving"
+      selectors = [
+        { namespace = "knative-serving" }
+      ]
+
+      # Using specific subnets instead of the subnets supplied for the cluster itself
+      subnet_ids = [module.vpc.private_subnets[1]]
+
+      tags = {
+	lf-cluster = local.name
+	GithubRepo = "localnet-farm"
+	GithubOrg  = "jimpick"
+      }
+
+      timeouts = {
+        create = "20m"
+        delete = "20m"
+      }
+    }
   }
 
   tags = local.tags
