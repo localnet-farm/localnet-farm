0be8d6fcf003f50f6c211eed97a4748a64a62604
Jim Pick <jim@jimpick.com>
2022-12-12 11:52:50 -0800

    Add Knative serving-core and service-crds

---
+++ prototypes/localnet-farm-3/argocd/knative/download.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+wget -r https://github.com/knative/serving/releases/download/knative-v1.8.3/serving-crds.yaml
+
+wget -r https://github.com/knative/serving/releases/download/knative-v1.8.3/serving-core.yaml
+
