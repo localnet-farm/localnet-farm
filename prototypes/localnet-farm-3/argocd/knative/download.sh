24b6461f9d85106ac52707e385d2ea191b9c5920
Jim Pick <jim@jimpick.com>
2022-12-12 15:17:08 -0800

    Try Kourier as networking layer for Knative

--- prototypes/localnet-farm-3/argocd/knative/download.sh
+++ prototypes/localnet-farm-3/argocd/knative/download.sh
@@ -4,3 +4,5 @@ wget -r https://github.com/knative/serving/releases/download/knative-v1.8.3/serv
 
 wget -r https://github.com/knative/serving/releases/download/knative-v1.8.3/serving-core.yaml
 
+wget -r https://github.com/knative/net-kourier/releases/download/knative-v1.8.1/kourier.yaml
+
