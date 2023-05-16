e0558e49fe64d22ae89190c322c11a53f86fe852
Jim Pick <jim@jimpick.com>
2023-05-16 16:52:38 -0700

    Add argocd-knative for cluster-4

---
+++ prototypes/localnet-farm-4/argocd-knative/knative/download.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+wget -r https://github.com/knative/serving/releases/download/knative-v1.8.3/serving-crds.yaml
+
+wget -r https://github.com/knative/serving/releases/download/knative-v1.8.3/serving-core.yaml
+
+wget -r https://github.com/knative/net-kourier/releases/download/knative-v1.8.1/kourier.yaml
+
