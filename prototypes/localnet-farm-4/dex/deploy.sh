4ebed33a5474be474a824c056f68ebf0b3dbdb18
Jim Pick <jim@jimpick.com>
2023-09-15 17:39:22 -0700

    Fix app name

--- prototypes/localnet-farm-4/dex/deploy.sh
+++ prototypes/localnet-farm-4/dex/deploy.sh
@@ -6,7 +6,7 @@ echo GITHUB_PAT $GITHUB_PAT
 
 argocd repo add https://github.com/jimpick/localnet-farm.git --username jimpick --password $GITHUB_PAT --upsert
 
-argocd app create localnet-farm-4-workloads \
+argocd app create localnet-farm-4-dex \
   --repo https://github.com/jimpick/localnet-farm.git \
   --path prototypes/localnet-farm-4/dex \
   --dest-name localnet-farm-4 \
