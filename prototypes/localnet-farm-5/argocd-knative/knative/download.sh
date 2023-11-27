9ddec88dc1f1f79093dc825fc9181ea3b51d9c36
update-image-hash <nobody@jimpick.com>
2023-11-26 23:00:23 -0800

    Update knative to v1.12.1

--- prototypes/localnet-farm-5/argocd-knative/knative/download.sh
+++ prototypes/localnet-farm-5/argocd-knative/knative/download.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-wget -r https://github.com/knative/serving/releases/download/knative-v1.10.1/serving-crds.yaml
+wget -r https://github.com/knative/serving/releases/download/knative-v1.12.1/serving-crds.yaml
 
-wget -r https://github.com/knative/serving/releases/download/knative-v1.10.1/serving-core.yaml
-
-wget -r https://github.com/knative/net-kourier/releases/download/knative-v1.10.0/kourier.yaml
+wget -r https://github.com/knative/serving/releases/download/knative-v1.12.1/serving-core.yaml
 
 wget -r https://github.com/knative/net-contour/releases/download/knative-v1.12.1/contour.yaml
 
