68a53bb4b520500656c72d6bfbe16915e756ad50
Jim Pick <nobody@jimpick.com>
2023-12-17 21:05:01 -0800

    Add cert-manager

---
+++ prototypes/localnet-farm-5/argocd-cert-manager/deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+. ../../../.env
+
+echo GITHUB_PAT $GITHUB_PAT
+
+argocd repo add https://github.com/jimpick/localnet-farm.git --username jimpick --password $GITHUB_PAT --upsert
+
+argocd app create localnet-farm-5-cert-manager \
+  --repo https://github.com/jimpick/localnet-farm.git \
+  --path prototypes/localnet-farm-5/argocd-cert-manager \
+  --dest-name localnet-farm-5 \
+  --dest-namespace default
