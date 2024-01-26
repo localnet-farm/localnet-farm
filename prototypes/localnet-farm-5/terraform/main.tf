f62c7dd6dab7cc2a7410f8c8cc18c8f0610a70b3
Jim Pick <nobody@jimpick.com>
2024-01-25 18:06:08 -0800

    Try to use spot instance

--- prototypes/localnet-farm-5/terraform/main.tf
+++ prototypes/localnet-farm-5/terraform/main.tf
@@ -99,7 +99,8 @@ module "eks" {
     # https://instances.vantage.sh/aws/ec2/t3a.xlarge?selected=m6a.large&region=us-east-1&os=linux&cost_duration=monthly&reserved_term=Standard.noUpfront
     # instance_types = ["t3a.xlarge"] # 4vCPUs, 16GiB, $109.79/mth
     instance_types = ["t3a.large"] # 2vCPUs, 8GiB, $54.896/mth
-  
+
+    capacity_type = "SPOT"
 
 
     # Force gp3 & encryption (https://github.com/bottlerocket-os/bottlerocket#default-volumes)
