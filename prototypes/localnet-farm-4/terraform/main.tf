7bb278dd0d07d1b03b40b4c51ba2bc34dd5732a1
Jim Pick <jim@jimpick.com>
2024-05-14 13:45:52 -0700

    More fixes 4

--- prototypes/localnet-farm-4/terraform/main.tf
+++ prototypes/localnet-farm-4/terraform/main.tf
@@ -429,45 +429,45 @@ resource "kubernetes_secret" "localnet_farm" {
   }
 }
 
-resource "helm_release" "lb" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  namespace  = "kube-system"
-  depends_on = [
-    kubernetes_service_account.service-account
-  ]
-
-  set {
-    name  = "region"
-    value = local.region
-  }
-
-  set {
-    name  = "vpcId"
-    value = module.vpc.vpc_id
-  }
-
-  set {
-    name  = "image.repository"
-    value = "602401143452.dkr.ecr.us-west-2.amazonaws.com/amazon/aws-load-balancer-controller"
-  }
-
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
-
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
-
-  set {
-    name  = "clusterName"
-    value = local.name
-  }
-}
+#resource "helm_release" "lb" {
+#  name       = "aws-load-balancer-controller"
+#  repository = "https://aws.github.io/eks-charts"
+#  chart      = "aws-load-balancer-controller"
+#  namespace  = "kube-system"
+#  depends_on = [
+#    kubernetes_service_account.service-account
+#  ]
+#
+#  set {
+#    name  = "region"
+#    value = local.region
+#  }
+#
+#  set {
+#    name  = "vpcId"
+#    value = module.vpc.vpc_id
+#  }
+#
+#  set {
+#    name  = "image.repository"
+#    value = "602401143452.dkr.ecr.us-west-2.amazonaws.com/amazon/aws-load-balancer-controller"
+#  }
+#
+#  set {
+#    name  = "serviceAccount.create"
+#    value = "false"
+#  }
+#
+#  set {
+#    name  = "serviceAccount.name"
+#    value = "aws-load-balancer-controller"
+#  }
+#
+#  set {
+#    name  = "clusterName"
+#    value = local.name
+#  }
+#}
 
 # IRSA role for EBS
 # https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
