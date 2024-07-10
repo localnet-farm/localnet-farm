54f6ae93605ca4c78a9f50d76859c524e7450efb
Jim Pick <jim@jimpick.com>
2024-07-09 19:02:58 -0700

    Define local.vpc_cidr

--- prototypes/localnet-farm-5/terraform/main.tf
+++ prototypes/localnet-farm-5/terraform/main.tf
@@ -5,6 +5,7 @@ locals {
   region          = "ca-central-1"
 
   azs = slice(data.aws_availability_zones.available.names, 0, 3)
+  vpc_cidr = "10.0.0.0/16"
 
   tags = {
     lf-cluster = local.name
