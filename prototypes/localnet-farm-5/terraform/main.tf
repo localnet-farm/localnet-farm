0dc50e83718bbc699d41e19e7354514a23d431f1
Jim Pick <jim@jimpick.com>
2024-07-14 14:33:39 -0700

    Re-add config to fetch token

--- prototypes/localnet-farm-5/terraform/main.tf
+++ prototypes/localnet-farm-5/terraform/main.tf
@@ -20,3 +20,12 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", local.name]
+    command     = "aws"
+  }
+}
