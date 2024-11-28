9856daf0f5a79e228aa0a4da03db79dc49f4483c
Jim Pick <jim@jimpick.com>
2024-11-27 19:09:18 -0800

    minikube6

---
+++ prototypes/minikube6/update-dns.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+CLUSTER=$(pwd | sed 's,^.*\/,,')
+IP=$(./get-ip.sh)
+
+echo IP: $IP
+
+export AWS_PROFILE=localnet_farm
+
+HOSTED_ZONE_ID=Z0936625SVJ16SI3AHDI
+
+JSON="$(cat <<EOF
+    {
+      "Changes": [
+        {
+          "Action": "UPSERT",
+          "ResourceRecordSet": {
+            "Name": "$CLUSTER.localnet.farm",
+            "Type": "A",
+            "TTL": 30,
+            "ResourceRecords": [
+              {
+                "Value": "$IP"
+              }
+            ]
+          }
+        },
+        {
+          "Action": "UPSERT",
+          "ResourceRecordSet": {
+            "Name": "*.$CLUSTER.localnet.farm",
+            "Type": "A",
+            "TTL": 30,
+            "ResourceRecords": [
+              {
+                "Value": "$IP"
+              }
+            ]
+          }
+        }
+      ]
+    }
+EOF
+)"
+
+echo $JSON
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $HOSTED_ZONE_ID \
+  --change-batch "$JSON" | cat
