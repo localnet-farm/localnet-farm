e23bd311587f7cfc52247545f3ab1fe973b93892
Jim Pick <jim@jimpick.com>
2024-11-28 19:47:28 -0800

    Remove minikube5

--- prototypes/minikube5/argocd-cert-manager/cert-manager/download.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-wget -r https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.yaml
-
--- prototypes/minikube5/argocd-knative/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-set -euo pipefail
-
-mkdir -p tmp
-
-kustomize build . > ./tmp/build.yaml
