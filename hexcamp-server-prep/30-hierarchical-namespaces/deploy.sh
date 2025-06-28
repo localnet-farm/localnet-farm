1dd1356adfcf6830bdc9ee9c14dfff6a28317142
Jim Pick <jim@jimpick.com>
2025-06-28 12:40:28 -0700

    Hierarchical namespaces

---
+++ hexcamp-server-prep/30-hierarchical-namespaces/deploy.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+. ../../.env
+
+CLUSTER=$1
+if [ -z "$CLUSTER" ]; then
+  echo Need cluster
+  exit 1
+fi
+
+echo GITHUB_PAT $GITHUB_PAT
+
+argocd repo add https://github.com/jimpick/localnet-farm.git --username jimpick --password $GITHUB_PAT --upsert
+
+argocd app create $CLUSTER-hierarchical-namespaces \
+  --upsert \
+  --repo https://github.com/jimpick/localnet-farm.git \
+  --path hexcamp-server-prep/30-hierarchical-namespaces \
+  --dest-name $CLUSTER \
+  --dest-namespace default
